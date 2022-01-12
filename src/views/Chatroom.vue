89673b6662ee17c7e6301aa06b7ffbaaa633312a
Aaron Alley <aaronalleydev@gmail.com>
2022-01-12 12:36:08 +0000

    uploaded and hosted on firebase

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,6 +1,7 @@
 <template>
 <div class="container">
   <Navbar />
+  <ChatWindow />
   <NewChatForm />
 </div>
   
@@ -9,12 +10,13 @@
 
 <script>
 import NewChatForm from '../components/NewChatForm.vue'
+import ChatWindow from '../components/ChatWindow.vue'
 import { watch } from '@vue/runtime-core'
 import Navbar from '../components/Navbar.vue'
 import getUser from '../composables/getUser'
 import { useRouter } from 'vue-router'
 export default {
-  components: { Navbar, NewChatForm},
+  components: { Navbar, NewChatForm, ChatWindow},
   setup() {
   
   const { user } = getUser()
